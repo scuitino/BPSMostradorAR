9bfb654eb089bb26f86efd5c3eef616e57b2b750
Seba GoRk <sebastiancuitino@gmail.com>
2017-09-24 22:55:20 -0300

    Ida y vuelta andando

---
+++ MostradorARProject/Assets/CMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMenuManager : MonoBehaviour {
+
+    [Header("ListItems"), SerializeField]
+    GameObject _itemsList;
+    [SerializeField]
+    List<GameObject> _itemInfoPanels;
+    GameObject _actualItem;
+
+    private void Start()
+    {
+        _actualItem = _itemsList;
+    }
+
+    public void OpenInfoPanel(int pSelectedItem)
+    {
+        _actualItem.SetActive(false);
+        _actualItem = _itemInfoPanels[pSelectedItem];
+        _actualItem.SetActive(true);
+    }
+
+    public void CloseInfoPanel()
+    {
+        _actualItem.SetActive(false);
+        _actualItem = _itemsList;
+        _actualItem.SetActive(true);
+    }
+}
